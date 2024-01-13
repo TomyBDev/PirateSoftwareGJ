2a582f635014bde306c1e168330ba0a2e52ed682
TomyB <123563755+TomyBDev@users.noreply.github.com>
2024-01-13 17:58:04 +0000

    New UE5 Engine Files

---
+++ Source/PirateSoftwareGJ/PirateSoftwareGJGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PirateSoftwareGJGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIRATESOFTWAREGJ_API APirateSoftwareGJGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
