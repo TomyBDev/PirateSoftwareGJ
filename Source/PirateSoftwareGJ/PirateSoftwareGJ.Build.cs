cd938f43e453632025c7170cc907c06a366833f2
TomyB <123563755+TomyBDev@users.noreply.github.com>
2024-01-14 00:03:52 +0000

    added simple ai

--- Source/PirateSoftwareGJ/PirateSoftwareGJ.Build.cs
+++ Source/PirateSoftwareGJ/PirateSoftwareGJ.Build.cs
@@ -8,7 +8,7 @@ public class PirateSoftwareGJ : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "EnhancedInput", "AIModule", "NavigationSystem" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
