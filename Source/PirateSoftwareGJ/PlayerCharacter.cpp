473d6931a0e462ac6cad11e961b00342b68962e7
TomyB <123563755+TomyBDev@users.noreply.github.com>
2024-01-20 22:22:29 +0000

    Constrained Camera Movement

--- Source/PirateSoftwareGJ/PlayerCharacter.cpp
+++ Source/PirateSoftwareGJ/PlayerCharacter.cpp
@@ -98,12 +98,16 @@ void APlayerCharacter::Look_Implementation(const FInputActionValue& Value)
 	// input is a Vector2D
 	FVector2D LookAxisVector = Value.Get<FVector2D>();
 
-	if (Controller != nullptr)
-	{
-		// add yaw and pitch input to controller
-		AddControllerYawInput(LookAxisVector.X);
-		AddControllerPitchInput(LookAxisVector.Y);
-	}
+	if (!IsValid(Controller))
+		return;
+
+	// add yaw and pitch input to controller
+	AddControllerPitchInput(LookAxisVector.Y);
+	AddControllerYawInput(LookAxisVector.X);
+
+	FRotator rot = GetController()->GetControlRotation();;
+	GetController()->SetControlRotation(FRotator(FMath::ClampAngle(FMath::UnwindDegrees(rot.Pitch), -30.f, 50.f), rot.Yaw, rot.Roll));
+	
 }
 
 void APlayerCharacter::Jump_Implementation()
