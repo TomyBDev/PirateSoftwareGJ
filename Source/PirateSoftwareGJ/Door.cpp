61dd4fa62ff5496a9394a787ee46f7acf3dc9f2c
TomyB <123563755+TomyBDev@users.noreply.github.com>
2024-01-21 01:16:36 +0000

    Added Door

---
+++ Source/PirateSoftwareGJ/Door.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2024 GalaxyCat Studio Inc. All rights reserved.
+
+
+#include "Door.h"
+
+#include "Components/BoxComponent.h"
+
+// Sets default values
+ADoor::ADoor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	collisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Root Comp"));
+	RootComponent = collisionBox;
+
+	mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Skeletal Mesh Component"));
+	mesh->SetupAttachment(collisionBox);
+}
+
+// Called when the game starts or when spawned
+void ADoor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADoor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ADoor::OpenDoor()
+{
+	if (IsValid(closeAnim))
+		mesh->PlayAnimation(closeAnim, false);
+
+	collisionBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
---
+++ Source/PirateSoftwareGJ/Door.h
@@ -0,0 +1,41 @@
+// Copyright (c) 2024 GalaxyCat Studio Inc. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Door.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class PIRATESOFTWAREGJ_API ADoor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADoor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	void OpenDoor();
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* collisionBox;
+
+	UPROPERTY(EditAnywhere)
+	USkeletalMeshComponent* mesh;
+
+	UPROPERTY(EditAnywhere)
+	UAnimationAsset* closeAnim;
+};
